fbbb508642ccc8df1bbf41d8e58282175c45a7de
Kim Dong Hun <hunkim98@gmail.com>
2022-08-25 21:54:24 +0900

    update git to act as library

--- prisma/migrations/20220825053723_add_telephone_to_user_model/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `telephone` to the `tb_user` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `tb_user` ADD COLUMN `deleted` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `telephone` VARCHAR(191) NOT NULL;
